71bbab454400c1799dfc3538157d1166c808b848
islom <islombekabilbekov@gmail.com>
2026-01-16 13:54:53 +0500

    First init

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        //1-zd
+        //        LinkedList<String> newArrayList = new LinkedList<>();
+//        newArrayList.add("A");
+//        newArrayList.add("B");
+//        newArrayList.add("C");
+//        newArrayList.add("D");
+//        newArrayList.add("E");
+//        newArrayList.add("F");
+//        newArrayList.add("G");
+//        newArrayList.add("H");
+//        newArrayList.add("I");
+//        newArrayList.add("J");
+//
+//        newArrayList.addFirst("K");
+//        newArrayList.addLast("L");
+//        System.out.println(newArrayList);
+
+//2-zd
+// linked list sekinro
+//        ArrayList<String> newArrayList = new ArrayList<>();
+//        newArrayList.add("A");
+//        newArrayList.add("B");
+//        newArrayList.add("C");
+//        newArrayList.add("D");
+//        newArrayList.add("E");
+//        newArrayList.add("F");
+//        newArrayList.add("G");
+//        newArrayList.add("H");
+//        newArrayList.add("I");
+//        newArrayList.add("J");
+//
+//        newArrayList.addFirst("K");
+//        newArrayList.addLast("L");
+//        System.out.println(newArrayList);
+
+
+
+//        array list tezro
+
+//        arrayda chunarliro i tezro
+//3-zd
+//        ArrayList<String> newArrayList = new ArrayList<>();
+        
+    }
+}
